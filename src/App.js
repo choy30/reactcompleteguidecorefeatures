8862ca2ec0fd3ae5512222fb5d4383fb81b5c414
devYeonjun <alcls0026@gmail.com>
2022-05-27 23:22:00 +1200

    Add modal componenet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import Backdrop from "./componenets/Backdrop";
+import Modal from "./componenets/Modal";
 import Todo from "./componenets/Todo";
 
 function App() {
 	return (
 		<div>
 			<h1>My Todos</h1>
-			<Todo text='Learn React' />
-			<Todo text='Master React' />
-			<Todo text='Explore the full React Course'/>
+			<Todo text="Learn React" />
+			<Todo text="Master React" />
+			<Todo text="Explore the full React Course" />
+			<Modal />
+			<Backdrop />
 		</div>
 	);
 }
