0d273b85d1ba71143c2927a574edb4b774b6fc1f
devYeonjun <alcls0026@gmail.com>
2022-05-28 00:13:08 +1200

    Add more components

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import MeetupList from "../componenets/meetups/MeetupList";
+
 const DUMMY_DATA = [
 	{
 		id: "m1",
@@ -25,11 +27,7 @@ const AllMeetups = () => {
 	return (
 		<section>
 			<h1>All Meetup</h1>
-			<ul>
-				{DUMMY_DATA.map((meetup) => {
-					return <li key={meetup.id}>{meetup.title}</li>;
-				})}
-			</ul>
+			<MeetupList meetups={DUMMY_DATA} />
 		</section>
 	);
 };
