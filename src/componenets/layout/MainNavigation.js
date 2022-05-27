aef5d1dcd5e4c3b356f9c35df9de84b02ddbc439
devYeonjun <alcls0026@gmail.com>
2022-05-27 23:58:21 +1200

    Add css

--- src/componenets/layout/MainNavigation.js
+++ src/componenets/layout/MainNavigation.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
-  return (
-    <header>
-        <div>
-            <ul>
-                <li>
-                    <Link to='/'>All Meetup</Link>
-                </li>
-                <li>
-                    <Link to='/new-meetup'>New Meetup</Link>
-                </li>
-                <li>
-                    <Link to='/favorite'>Favorite</Link>
-                </li>
-            </ul>
-        </div>
-    </header>
-  )
-}
+	return (
+		<header className={classes.header}>
+			<div className={classes.logo}>React Meetups</div>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">All Meetup</Link>
+					</li>
+					<li>
+						<Link to="/new-meetup">New Meetup</Link>
+					</li>
+					<li>
+						<Link to="/favorite">Favorite</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
 
-export default MainNavigation
+export default MainNavigation;
