3099c9273b7e18325d76094f552f189e58fdd6e4
devYeonjun <alcls0026@gmail.com>
2022-05-27 23:13:09 +1200

    Add building & re-using another componenet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import Todo from "./componenets/Todo";
+
 function App() {
 	return (
 		<div>
 			<h1>My Todos</h1>
-			<div className="card">
-				<h2>Title</h2>
-				<div>
-					<button>Delete</button>
-				</div>
-			</div>
+			<Todo />
+			<Todo />
+			<Todo />
 		</div>
 	);
 }
