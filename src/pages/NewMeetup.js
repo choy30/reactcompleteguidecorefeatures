54e3d3f2919d659575600e1a9167a6ef812bff9d
devYeonjun <alcls0026@gmail.com>
2022-05-28 00:59:29 +1200

    Add post request to firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -3,14 +3,20 @@ import React from "react";
 import NewMeetupForm from "../componenets/meetups/NewMeetupForm";
 
 const NewMeetup = () => {
-    const addMettupHandler = (meetupData) => {
-
-    }
+	const addMettupHandler = (meetupData) => {
+		fetch("https://react-http-a1d35-default-rtdb.firebaseio.com/meetups.json", {
+			method: "POST",
+			body: JSON.stringify(meetupData),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	};
 
 	return (
 		<section>
 			<h1>Add New Meetup</h1>
-			<NewMeetupForm onAddMeetup={addMettupHandler}/>
+			<NewMeetupForm onAddMeetup={addMettupHandler} />
 		</section>
 	);
 };
