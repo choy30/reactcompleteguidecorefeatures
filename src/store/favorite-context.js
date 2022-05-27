78b83c8c8413bd59f450dfef55138614c397b872
devYeonjun <alcls0026@gmail.com>
2022-05-28 01:27:27 +1200

    Add context logic

--- src/store/favorite-context.js
+++ src/store/favorite-context.js
@@ -1,19 +1,37 @@
 import { createContext, useState } from "react";
 
 const FavoriteContext = createContext({
-    favorites: [],
-    totalFavorites: 0
-})
+	favorites: [],
+	totalFavorites: 0,
+});
 
 const FavoritesContextProvider = (props) => {
-    const [userFavorites, setUserFavorites] = useState([]);
+	const [userFavorites, setUserFavorites] = useState([]);
 
-    const context = {
-        favorites: userFavorites,
-        totalFavorites: userFavorites.length
-    };
+	const addFavoriteHandler = (favoriteMeetup) => {
+		setUserFavorites((prevUserFavorites) => {
+			return prevUserFavorites.concat(favoriteMeetup);
+		});
+	};
 
-    return <FavoriteContext.Provider value={context}>
-        {props.children}
-    </FavoriteContext.Provider>
-}
+	const removeFavoriteHandler = (meetupId) => {
+		setUserFavorites((prevUserFavorites) => {
+			return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+		});
+	};
+
+	const itemIsFavoriteHandler = (meetupId) => {
+		return userFavorites.some((meetup) => meetup.id === meetupId);
+	};
+
+	const context = {
+		favorites: userFavorites,
+		totalFavorites: userFavorites.length,
+	};
+
+	return (
+		<FavoriteContext.Provider value={context}>
+			{props.children}
+		</FavoriteContext.Provider>
+	);
+};
